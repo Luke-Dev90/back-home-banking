3712623dc110b35c6b33cadf832e760c813095ae
Luke-Dev90 <lukee_sf@hotmail.com>
2021-09-01 09:20:50 -0300

    init home banking

---
+++ src/main/java/com/lchalela/banking/BackendBankOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.lchalela.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBankOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBankOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lchalela/banking/BackendBankOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lchalela.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBankOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
